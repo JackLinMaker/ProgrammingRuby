ce8677b94c5a75f189f776c2611a1af7511db643
jacklinmaker@gmail.com <jacklinmaker@gmail.com>
2013-08-11 22:58:28 +0800

    finish Expression

---
+++ Expression/Bowdlerize.rb
@@ -0,0 +1,17 @@
+class Bowdlerize
+  def initialize(string)
+    @value = string.gsub(/[aeiou]/, '*')
+  end
+  
+  def +(other)
+    Bowdlerize.new(self.to_s + other.to_s)
+  end
+  
+  def to_s
+    @value
+  end
+end
+
+a = Bowdlerize.new("damn")
+a += "shame"
+puts a
